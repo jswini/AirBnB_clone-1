393e3140a97bbe27994c6bc7178d2efeef51f091
jswini <jswinikoff@gmail.com>
2021-12-14 13:56:20 -0600

    moved comment to correct location

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-from flask import Flask
-app = Flask(__name__)
 '''
 this script creates a page to return hello hbnb
 '''
+from flask import Flask
+app = Flask(__name__)
 
 
 @app.route('/')
