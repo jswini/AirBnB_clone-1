92c015f3fe94dfa5f39b61173d265bee09a57bf0
jswini <jswinikoff@gmail.com>
2021-12-14 14:08:26 -0600

    first commit of 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''
+this script creates a page to return hello hbnb
+'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_HBNB():
+    '''This method creates a route and gives it some text'''
+    strict_slashes = False
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def HBNB():
+    '''this method routes to /hbnb'''
+    strict_slashes = False
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    '''this method prints the text from the var passed'''
+    return 'c %s' % text
+
+
+if __name__ == '__main__':
+    '''this method sets everything to work on 0.0.0.0'''
+    app.debug = True
+    app.run(host="0.0.0.0")
