5e0a57a05bd321dc1f8115045ff0de7ab354e6dc
jswini <jswinikoff@gmail.com>
2021-12-14 13:48:49 -0600

    fixed for linter

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 from flask import Flask
 app = Flask(__name__)
+'''
+this script creates a page to return hello hbnb
+'''
+
 
 @app.route('/')
 def hello_HBNB():
-    strict_slashes=False
+    '''This method creates a route and gives it some text'''
+    strict_slashes = False
     return 'Hello HBNB!'
 
+
 if __name__ == '__main__':
+    '''this method sets everything to work on 0.0.0.0'''
     app.debug = True
     app.run(host="0.0.0.0")
