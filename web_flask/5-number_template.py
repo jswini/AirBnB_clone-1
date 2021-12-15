b67fc6d588f525a05066c3cceecff191dae2d4bc
jswini <jswinikoff@gmail.com>
2021-12-15 10:22:05 -0600

    moved strict slash to route template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -46,10 +46,9 @@ def number_text(n):
     return ('{} is a number'.format(n))
 
 
-@app.route('/number_template/<int:n>')
+@app.route('/number_template/<int:n>', strict_slashes = False)
 def number_html(n=None):
     '''this method displays a website from a template'''
-    #strict_slashes = False
     return render_template('5-number.html', n=n)
 
 
