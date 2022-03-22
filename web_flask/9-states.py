454c2b15a5fa82170002b6d3218241bfcbc167ce
jswini <jswinikoff@gmail.com>
2022-03-22 14:39:43 -0500

    adjusted formatting of output

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -28,20 +28,9 @@ def states(id=None):
     if id:
         search_state = state.get("State." + id)
         state = None
-    return render_template('/9-states.html', State=state, search_state=search_state)
+    return render_template('/9-states.html', State=state,
+                           search_state=search_state)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-'''
-for 11 app.route('/hbnb_filters)
-def something()"
-storage var create
-return
-'''
-""" <!--for task 11 html
-    change hard code values in webstatic 6-index
-to use vars from the python code --> """
